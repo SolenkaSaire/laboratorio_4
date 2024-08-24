e0d77095a471312bfb42c06a846c224c15809a10
SolenkaSaire <solenkam.sairec@uqvirtual.edu.co>
2024-08-24 09:30:50 -0500

    symmertriccipher update

--- src/symmetriccipher/SymmetricCipherTester01.java
+++ src/symmetriccipher/SymmetricCipherTester01.java
@@ -20,11 +20,16 @@ public class SymmetricCipherTester01 {
         // se crea el objeto de cifrado simetrico con la llave secreta y el algoritmo
         SymmetricCipher cipher = new SymmetricCipher(secretKey, "DES/ECB/PKCS5Padding");
 
-        String clearText = "In symmetric key cryptography, the same key is used to encrypt and decrypt the clear text.";
+       // String clearText = "In symmetric key cryptography, the same key is used to encrypt and decrypt the clear text.";
+        String clearText = "literalmente ryan gosling";
+
         System.out.println(clearText);
 
         // se cifra el mensaje claro
         byte[] encryptedText = cipher.encryptMessage(clearText);
+
+        //se imprime el mensaje cifraddo
+        System.out.println(new String(encryptedText));
         // se imprime el mensaje cifrado en hexadecimal
         System.out.println(Util.byteArrayToHexString(encryptedText, " "));
 
